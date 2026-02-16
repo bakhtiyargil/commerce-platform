5b2f3eaee7264b01c692b7c059e99c849a7a30a3
Bakhtiyar V. Karimov <bext97@mail.ru>
2026-02-16 15:28:01 +0400

    error codes fix

--- ms-order/src/main/java/az/baxtiyargil/commerce/order/domain/error/EntityNotFoundErrorCodes.java
+++ ms-order/src/main/java/az/baxtiyargil/commerce/order/domain/error/EntityNotFoundErrorCodes.java
@@ -6,17 +6,17 @@ import org.springframework.http.HttpStatus;
 
 public enum EntityNotFoundErrorCodes implements ErrorCode {
 
-    PRODUCT_NOT_FOUND("PRODUCT_NOT_FOUND", HttpStatus.NOT_FOUND, RetryPolicy.NON_RETRYABLE),
-    CUSTOMER_NOT_FOUND("CUSTOMER_NOT_FOUND", HttpStatus.NOT_FOUND, RetryPolicy.NON_RETRYABLE),
-    STORE_NOT_FOUND("STORE_NOT_FOUND", HttpStatus.NOT_FOUND, RetryPolicy.NON_RETRYABLE);
+    PRODUCT_NOT_FOUND("PRODUCT_NOT_FOUND", RetryPolicy.NON_RETRYABLE),
+    CUSTOMER_NOT_FOUND("CUSTOMER_NOT_FOUND", RetryPolicy.NON_RETRYABLE),
+    STORE_NOT_FOUND("STORE_NOT_FOUND", RetryPolicy.NON_RETRYABLE);
 
     private final String message;
     private final HttpStatus status;
     private final RetryPolicy retryPolicy;
 
-    EntityNotFoundErrorCodes(String message, HttpStatus httpStatus, RetryPolicy retryPolicy) {
+    EntityNotFoundErrorCodes(String message, RetryPolicy retryPolicy) {
         this.message = message;
-        this.status = httpStatus;
+        this.status = HttpStatus.NOT_FOUND;
         this.retryPolicy = retryPolicy;
     }
 
