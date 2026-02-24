09a79db2485ac33866f3fa66a0bacd6fc3342fba
Bakhtiyar V. Karimov <bext97@mail.ru>
2026-02-24 12:54:25 +0400

    unused import removed

--- common/lib-security/src/main/java/az/baxtiyargil/commerce/lib/security/component/SecurityExceptionHandler.java
+++ common/lib-security/src/main/java/az/baxtiyargil/commerce/lib/security/component/SecurityExceptionHandler.java
@@ -11,7 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.Instant;
