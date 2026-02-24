1aaff91d9801b21b67ef7acd54e47fb832eee02c
Bakhtiyar V. Karimov <bext97@mail.ru>
2026-02-24 17:09:05 +0400

    auth service security config correction

--- ms-auth/src/main/java/az/baxtiyargil/commerce/auth/infrastructure/SecurityConfiguration.java
+++ ms-auth/src/main/java/az/baxtiyargil/commerce/auth/infrastructure/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package az.baxtiyargil.commerce.auth.infrastructure;
 
-import az.baxtiyargil.commerce.lib.security.component.SecurityExceptionHandler;
+import az.baxtiyargil.commerce.lib.security.component.BaseSecurityConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,24 +15,19 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration {
 
     private final ApiKeyAuthFilter apiKeyAuthFilter;
-    private final SecurityExceptionHandler securityExceptionHandler;
+    private final BaseSecurityConfiguration baseSecurityConfiguration;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        baseSecurityConfiguration.applyCommonSecurity(http);
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/api/auth/**").permitAll()
                         .requestMatchers("/v1/api/internal/**").permitAll()
                         .requestMatchers("/actuator/health").permitAll()
                         .anyRequest().denyAll()
                 )
-                .addFilterBefore(apiKeyAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(handler -> {
-                    handler.accessDeniedHandler(securityExceptionHandler::handleAccessDenied);
-                    handler.authenticationEntryPoint(securityExceptionHandler::handleAuthenticationError);
-                });
+                .addFilterBefore(apiKeyAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
